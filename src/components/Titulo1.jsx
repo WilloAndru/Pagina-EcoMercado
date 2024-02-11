2255f64ec8a09f246ddd0ec7d4113b2c421d4729
Willo <wilsonandrescriollo@gmail.com>
2024-02-11 13:40:29 -0500

    Mi primer commit

---
+++ src/components/Titulo1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Titulo1() {
+  return (
+    <h1 id="titulo1">Compra y vende artículos sostenibles en EcoMercado, una comunidad comprometida con el medio ambiente.</h1>
+  )
+}
+
+export default Titulo1
